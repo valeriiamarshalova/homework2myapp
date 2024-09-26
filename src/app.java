660a5850cf67353c350ca481faae61c7df2cf67a
valeriiamarshalova <leramarshalova@gmail.com>
2024-09-27 00:24:17 +0200

    modificated greeting message

--- src/app.java
+++ src/app.java
@@ -2,7 +2,7 @@ public class app {
 
     public static void main(String[] args){
 
-        System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
 
     }
 
